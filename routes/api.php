2f21ae89562a72e9b6f8c3cd5ce6e8ed2c846ad9
Himanshu Khudania <hkhudania@gmail.com>
2020-07-22 21:13:35 +0530

    Added Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//List Articles
+Route::get('articles', 'ArticleController@index');
+
+//Single Article
+Route::get('article/{id}', 'ArticleController@show');
+
+//Create new Articles
+Route::post('article', 'ArticleController@store');
+
+//UPdate Articles
+Route::put('articles', 'ArticleController@store');
+
+//Delete Articles
+Route::delete('articles', 'ArticleController@destory');
